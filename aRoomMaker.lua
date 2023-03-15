62142b56e286291f329cefdc2a7c64e89767ba14
xJ3FFYx <47201578+xJ3FFYx@users.noreply.github.com>
2023-03-14 20:26:15 -0400

    Update aRoomMaker.lua
    Test

--- aRoomMaker.lua
+++ aRoomMaker.lua
@@ -4,6 +4,7 @@ print("Welcome the the Room Builder")
 
 function moveForward(x)
     for i = 0, x, +1 do
-        print(Moving UP one)
+        print("Moving UP one")
+        print(x)
     end
 end
