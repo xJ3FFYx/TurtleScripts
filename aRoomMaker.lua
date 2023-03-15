088cb500274f71d775424104aaadd290f2d5cba4
xJ3FFYx <47201578+xJ3FFYx@users.noreply.github.com>
2023-03-14 20:39:48 -0400

    Update aRoomMaker.lua

--- aRoomMaker.lua
+++ aRoomMaker.lua
@@ -3,8 +3,11 @@ print("Welcome the the Room Builder")
 
 
 function moveForward(x)
-    for i = 0, x, +1 do
-        print("Moving UP one")
-        print(x)
+    for i = 0,10 do
+        while not turtle.forward() do 
+            turtle.dig()
+        end
     end
 end
+
+moveForward(5)
